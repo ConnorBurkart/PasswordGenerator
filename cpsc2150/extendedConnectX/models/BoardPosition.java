151bad1a436995c54b1074876649d197fef22220
Jacob Colson <jacobcolson02@yahoo.com>
2023-09-19 11:09:31 -0400

    Updated BoardPosition Contracts

--- cpsc2150/extendedConnectX/models/BoardPosition.java
+++ cpsc2150/extendedConnectX/models/BoardPosition.java
@@ -15,23 +15,41 @@ public class BoardPosition
      * Constructs a BoardPosition object that indicates the exact
      * position of a token within the 9x7 range of the board.
      *
-     * @param
-     * @param
-     * @pre
-     * aRow > 0 and aRow <= 9
-     * aColumn > 0 and aColumn <= 7
-     * @post
+     * @param aRow
+     * @param aColumn
+     *
+     * @pre aRow > 0 AND aRow <= 9 AND aColumn > 0 AND aColumn <= 7
+     *
+     * @post Row = aRow AND Column = aColumn
      */
     public BoardPosition(int aRow, int aColumn)
     {
         //parameterized constructor for BoardPosition
     }
 
+    /**
+     * returns the row number for BoardPosition
+     *
+     * @return The row number as an integer.
+     *
+     * @pre None
+     * 
+     * @post getRow = Row. Row = #Row AND Column = #Column
+     */
     public int getRow()
     {
         //returns the row
     }
 
+    /**
+     * returns the column number for BoardPosition
+     *
+     * @return The column number as an integer.
+     *
+     * @pre None
+     * 
+     * @post getColumn = Column. Row = #Row AND Column = #Column
+     */
     public int getColumn()
     {
         //returns the column
