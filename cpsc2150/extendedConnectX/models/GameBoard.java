453bdc5363799d2938f1d086fa50156c399ce249
Jacob Colson <jacobcolson02@yahoo.com>
2023-11-01 15:51:15 -0400

    removed overriden toString from GameBoard

--- cpsc2150/extendedConnectX/models/GameBoard.java
+++ cpsc2150/extendedConnectX/models/GameBoard.java
@@ -94,43 +94,7 @@ public class GameBoard extends AbsGameBoard implements IGameBoard
         }
 
     }
-
-    /**
-     * Creates a string for the GameBoard containing each value within 2D gameboard array.
-     *
-     * @return the GameBoard array as a String.
-     *
-     * @pre None
-     *
-     * @post [toString returns each value contained within the 2D GameBoard array
-     * as a string formatted as the GameBoard] AND ROWS = #ROWS AND COLUMNS = #COLUMNS AND numToWin = #numToWin
-     * AND self = #self
-     * 
-     */
-    /*
-    @Override
-    public String toString()
-    {
-        //Begins string with header for the output GameBoard
-        String boardString = "|0|1|2|3|4|5|6|\n";
-
-        for (int i = getNumRows() - 1; i >= 0; i--) {
-            for (int j = 0; j < getNumColumns(); j++) {
-                //Creates a new BoardPosition for current spot on GameBoard
-                BoardPosition currentPos = new BoardPosition(i, j);
-                boardString += ('|');
-                boardString += (whatsAtPos(currentPos));
-            }
-            boardString += ('|');
-            boardString += '\n';
-        }
-
-        //Returns the generated String
-        return boardString;
-    }
-     */
-
-
+    
     /**
      * Returns the value for ROWS
      *
