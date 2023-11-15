d791430d63d80f4408d111b07f93814f04918f76
Jacob Colson <jacobcolson02@yahoo.com>
2023-11-15 17:21:25 -0500

    updated GameBoard tests

--- cpsc2150/extendedConnectX/tests/TestGameBoard.java
+++ cpsc2150/extendedConnectX/tests/TestGameBoard.java
@@ -1,13 +1,50 @@
 package cpsc2150.extendedConnectX.tests;
 
 import cpsc2150.extendedConnectX.models.GameBoard;
+import cpsc2150.extendedConnectX.models.IGameBoard;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class TestGameBoard {
 
-    private GameBoard GameBoardFactory(int aRow, int aColumn, int numWin) {
+    private IGameBoard GameBoardFactory(int aRow, int aColumn, int numWin) {
         return new GameBoard(aRow, aColumn, numWin);
     }
+
+    private String makeExpectedGameBoard(char[][] aArray, int numRows, int numColumns) {
+        String expectedBoard = "";
+        int DoubleDigit = 10;
+
+        for (int i = 0; i < numColumns; i++) {
+            if (i < DoubleDigit) {
+                expectedBoard += "| ";
+            }
+            else {
+                expectedBoard += '|';
+            }
+            expectedBoard += i;
+        }
+
+        expectedBoard += "|\n";
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColumns; j++) {
+                expectedBoard += '|';
+
+                if (!Character.isLetter(aArray[i][j])) {
+                    expectedBoard += "  ";
+                }
+                else {
+                    expectedBoard += aArray[i][j];
+                    expectedBoard += ' ';
+                }
+            }
+            expectedBoard += "|\n";
+        }
+
+        return expectedBoard;
+    }
+
+    //Constructor Tests
 
     @Test
     public void test_5_10_5_GameBoard() {
@@ -15,19 +52,32 @@ public class TestGameBoard {
         int numRows = 5;
         int numColumns = 10;
         int numToWin = 5;
-        GameBoard testBoard = GameBoardFactory(numRows, numColumns, numToWin);
 
-        assertTrue(testBoard.getNumRows() == numRows && testBoard.getNumColumns() == numColumns && testBoard.getNumToWin() == numToWin);
+        char[][] expectedBoard = new char[numRows][numColumns];
+        IGameBoard gb = GameBoardFactory(numRows, numColumns, numToWin);
+
+        String blankBoard = makeExpectedGameBoard(expectedBoard, numRows, numColumns);
+
+        assertEquals(numRows, gb.getNumRows());
+        assertEquals(numColumns, gb.getNumColumns());
+        assertTrue(gb.toString().equals(blankBoard));
     }
+
 
     @Test
     public void test_75_4_75_GameBoard() {
         int numRows = 75;
         int numColumns = 4;
         int numToWin = 75;
-        GameBoard testBoard = GameBoardFactory(numRows, numColumns, numToWin);
 
-        assertTrue(testBoard.getNumRows() == numRows && testBoard.getNumColumns() == numColumns && testBoard.getNumToWin() == numToWin);
+        char[][] expectedBoard = new char[numRows][numColumns];
+        IGameBoard gb = GameBoardFactory(numRows, numColumns, numToWin);
+
+        String blankBoard = makeExpectedGameBoard(expectedBoard, numRows, numColumns);
+
+        assertEquals(numRows, gb.getNumRows());
+        assertEquals(numColumns, gb.getNumColumns());
+        assertTrue(gb.toString().equals(blankBoard));
     }
 
     @Test
@@ -35,11 +85,67 @@ public class TestGameBoard {
         int numRows = 100;
         int numColumns = 100;
         int numToWin = 25;
-        GameBoard testBoard = GameBoardFactory(numRows, numColumns, numToWin);
 
-        assertTrue(testBoard.getNumRows() == numRows && testBoard.getNumColumns() == numColumns && testBoard.getNumToWin() == numToWin);
+        char[][] expectedBoard = new char[numRows][numColumns];
+        IGameBoard gb = GameBoardFactory(numRows, numColumns, numToWin);
+
+        String blankBoard = makeExpectedGameBoard(expectedBoard, numRows, numColumns);
+
+        assertEquals(numRows, gb.getNumRows());
+        assertEquals(numColumns, gb.getNumColumns());
+        assertTrue(gb.toString().equals(blankBoard));
     }
 
-    //Constructor tests above
+    //CheckIfFree tests
+    @Test
+    public void test_3_empty_CheckIfFree() {
+        int numRows = 4;
+        int numColums = 10;
+        int numToWin = 5;
+
+        int ColumnToDrop = 3;
+        IGameBoard gb = GameBoardFactory(numRows, numColums, numToWin);
+
+        assertTrue(gb.checkIfFree(ColumnToDrop));
+    }
+
+    @Test
+    public void test_0_half_CheckIfFree() {
+        int numRows = 4;
+        int numColums = 10;
+        int numToWin = 5;
+
+        int ColumnToDrop = 0;
+        IGameBoard gb = GameBoardFactory(numRows, numColums, numToWin);
+
+        for (int i = 0; i < (numRows / 2); i++) {
+            gb.dropToken('X', ColumnToDrop);
+        }
+
+        assertTrue(gb.checkIfFree(ColumnToDrop));
+    }
+
+    @Test
+    public void test_2_fullBoard_CheckIfFree() {
+        int numRows = 4;
+        int numColums = 10;
+        int numToWin = 5;
+
+        int ColumnToDrop = 2;
+        IGameBoard gb = GameBoardFactory(numRows, numColums, numToWin);
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColums; j++) {
+                gb.dropToken('X', j);
+            }
+        }
+
+        assertTrue(!gb.checkIfFree(ColumnToDrop));
+
+    }
+
+    //checkHorizWin tests
+
+
 
 }
--- cpsc2150/extendedConnectX/tests/TestGameBoardMem.java
+++ cpsc2150/extendedConnectX/tests/TestGameBoardMem.java
@@ -2,6 +2,8 @@ package cpsc2150.extendedConnectX.tests;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import cpsc2150.extendedConnectX.models.IGameBoard;
+import cpsc2150.extendedConnectX.models.GameBoardMem;
 public class TestGameBoardMem {
 
 
